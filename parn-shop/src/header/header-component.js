ce1f59864721731bacb1a8c7437f4276b0dade05
Dani Carter <carte470@d.umn.edu>
2019-09-10 21:30:14 -0500

    HeaderComponent added

---
+++ parn-shop/src/header/header-component.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./header-component.css";
+
+export const HeaderComponent = props => (
+  <div className="header">
+    <h4>Parnassus Preparatory School</h4>
+    <h1>Zeus's Store</h1>
+  </div>
+);
