cf7e37fa4450b39a25c6711dfc531c4bb35a2d94
Dani Carter <carte470@d.umn.edu>
2019-09-11 18:56:05 -0500

    footer added

--- parn-shop/src/App.js
+++ parn-shop/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { HeaderComponent } from "./header/header-component";
 import "./App.css";
+import { FooterComponent } from "./footer/footer-component";
 
 function App() {
   return (
     <div className="App">
       <HeaderComponent />
+      <FooterComponent />
     </div>
   );
 }
---
+++ parn-shop/src/footer/footer-component.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./footer-component.css";
+
+export const FooterComponent = props => (
+  <div className="footer">&copy; Dani Carter 2019</div>
+);
